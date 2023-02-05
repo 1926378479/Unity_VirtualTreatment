7d3dcaeec3d6cd4964cdfdc4148d7917d814bb43
FQ <1926378479@qq.com>
2023-02-05 14:52:39 +0800

    病人生成并行走到指定位置

--- Assets/Scripts/Manager/PatientManager.cs
+++ Assets/Scripts/Manager/PatientManager.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PatientManager : MonoBehaviour
 {
     public GameObject[] PatientGroup;
-    public Transform WalkStartPos;
-    public Transform WalkEndPos;
+    public Transform startPos;
+    public Transform turnPos;
+    public Transform endPos;
 
     public void CreatPatient()
     {
         GameObject Patient = PatientGroup[Random.Range(0, PatientGroup.Length)];
-        Instantiate(Patient, WalkStartPos.position,Quaternion.identity);
-        Debug.Log("创建成功");
+        GameObject currentPatient = Instantiate(Patient, startPos.position,Quaternion.identity);
+        currentPatient.transform.DOMove(turnPos.position, 2.5f).OnComplete(()=> {
+            currentPatient.GetComponent<Animator>().SetTrigger("Turn");
+            currentPatient.transform.DOMove(endPos.position, 2.5f).OnComplete(()=> {
+                currentPatient.GetComponent<Animator>().SetTrigger("Idle");
+            });
+            Debug.Log("创建成功");
+        });
     }
 
     private void Start()
     {
-        InvokeRepeating("CreatPatient", 0, 5);
+        InvokeRepeating("CreatPatient", 0, 6);
     }
 }
